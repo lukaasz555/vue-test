ace2c1bcc8ed86a93d8878dd2220e3e2b09b4d57
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-02-26 10:51:11 +0100

    first component and some styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,85 @@
 <template>
-	<p>12</p>
+	<h1>{{ message }}</h1>
+	<ul>
+		<SingleItem
+			v-for="item in storedResources"
+			v-bind="item"
+			:key="item.id"
+		></SingleItem>
+	</ul>
 </template>
 
 <script>
-export default {};
+import SingleItem from './components/SingleItem.vue';
+export default {
+	data() {
+		return {
+			message: 'test message',
+			storedResources: [
+				{
+					id: 0,
+					title: 'First item',
+					description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+					link: 'https://github.com/lukaasz555/vue-test',
+				},
+
+				{
+					id: 1,
+					title: 'Second one',
+					description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+					link: 'https://github.com/lukaasz555/vue-test',
+				},
+			],
+		};
+	},
+	components: {
+		SingleItem,
+	},
+};
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap');
+
+body {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Roboto', sans-serif;
+}
+
+*,
+*::before,
+*::after {
+	margin: 0;
+	padding: 0;
+	box-sizing: inherit;
+}
+
+ul {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 80%;
+	margin: 1em auto;
+}
+
+h1 {
+	text-align: center;
+	margin: 2em 0;
+}
+
+button {
+	border: none;
+	background: transparent;
+	cursor: pointer;
+}
+
+a {
+	text-decoration: none;
+	color: orange;
+}
+a:hover {
+	text-decoration: underline;
+}
+</style>
